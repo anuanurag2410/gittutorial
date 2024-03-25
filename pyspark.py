a1c00daccaf050333d05f65de0ff0cf16d476166
Anurag Srivastava <anuanurag2410@gmail.com>
2024-03-25 14:39:01 +0530

    Adding new pyspark feature

---
+++ pyspark.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, array_remove
+
+# Create SparkSession
+spark = SparkSession.builder \
+    .appName("Remove Nulls from Array") \
+    .getOrCreate()
+
+# Sample data
+data = [(1, [10, None, 30, None, 50]),
+        (2, [100, 200, None, 400]),
+        (3, [None, 2, 3, None, 5, 6, None, 8, 9, None])]
+
+# Create DataFrame
+df = spark.createDataFrame(data, ["id", "num"])
+
+# Remove null values from the "num" array column
+df_no_nulls = df.withColumn("num", array_remove(col("num"), None))
+
+# Show DataFrame
+df_no_nulls.show(truncate=False)
+
+# Stop SparkSession
+spark.stop()
